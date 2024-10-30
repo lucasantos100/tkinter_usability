20dfbc6f10490407d157b638028f0fed08c9e938
Lucas Santos <lucas_santos100@estudante.sesisenai.org.br>
2024-10-30 16:36:29 -0300

    Add files via upload
    Examples of how to use the tkinter library in python

---
+++ radiobutton.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title("AVIÃO")
+
+var_aviao = tk.StringVar()
+
+botao_classeeconomica = tk.Radiobutton(text="Classe Econômica", variable = var_aviao, value = "Classe Econômica")
+botao_classeexecutiva = tk.Radiobutton(text="Classe Executiva", variable = var_aviao, value = "Classe Executiva")
+botao_primeiraclasse = tk.Radiobutton(text="Primeira Classe", variable = var_aviao, value = "Primeira Classe")
+botao_classeeconomica.grid(row = 0, column = 0)
+botao_classeexecutiva.grid(row = 0, column = 1)
+botao_primeiraclasse.grid(row = 0, column = 2)
+
+def enviar():
+    print(var_aviao.get())
+    
+botao_enviar = tk.Button(text = "Enviar", command = enviar)
+botao_enviar.grid(row = 1, column = 0)
+
+tk.mainloop()
